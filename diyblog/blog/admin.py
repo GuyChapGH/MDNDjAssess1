ea62d855382e187e78b8e617aed299a23d4f2d52
Guy Chaplin <guy.chaplin@googlemail.com>
2024-04-24 12:58:30 +0100

    Add inline editing for blog comments

--- diyblog/blog/admin.py
+++ diyblog/blog/admin.py
@@ -3,8 +3,17 @@ from .models import Blog, Comment, Blogger
 
 # Register your models here.
 
-admin.site.register(Blog)
-admin.site.register(Comment)
+# admin.site.register(Blog)
+# admin.site.register(Comment)
 admin.site.register(Blogger)
 
 
+# Sets up Comments as inline editing on Blog in Admin site
+class CommentsInLine(admin.TabularInline):
+    model = Comment
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'author')
+
+    inlines = [CommentsInLine]

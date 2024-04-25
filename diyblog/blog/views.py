560dfdad06f10fe0b5ca1c3b1ac986769b097446
Guy Chaplin <guy.chaplin@googlemail.com>
2024-04-25 10:44:39 +0100

    Create index page, url and view

--- diyblog/blog/views.py
+++ diyblog/blog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
+from .models import Blog, Comment, Blogger
+
 # Create your views here.
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of blogs 
+    num_blogs = Blog.objects.all().count()
+
+    # Generate count of bloggers
+    num_bloggers = Blogger.objects.all().count()
+
+    context = {
+        'num_blogs': num_blogs,
+        'num_bloggers': num_bloggers,
+    }
+
+    # Render the HTML template index.html with the data in the context var
+    return render(request, 'index.html', context=context)
